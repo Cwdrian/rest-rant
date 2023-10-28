f5aa71384d4c30f58d67ef084de16c8f3e977af0
Cwdrian <xcharlesdrian@gmail.com>
2023-10-28 01:02:05 -0400

    New places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,54 +1,50 @@
 const router = require('express').Router()
-
+const places = require('../models/places.js')
 
 router.get('/', (req, res) => {
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: 'http://placekitten.com/250/250'
-    }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: 'http://placekitten.com/250/250'
-    }]
     res.render('places/index', { places })
 })
 
 router.post('/', (req, res) => {
-    res.send('POST /places stub')
+    if (!req.body.pic) {
+        req.body.pic = '/images/Error404.jpg'
+    }
+    if (!req.body.city) {
+        req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+        req.body.state = 'USA'
+    }
+    places.push(req.body)
+    res.redirect('/places')
 })
 
 router.get('/new', (req, res) => {
     res.render('places/new')
-  })
-  
+})
 
 router.get('/:id', (req, res) => {
-    res.send('GET /places/:id stub')
+    res.send('GET /places/:id')
 })
 
 router.put('/:id', (req, res) => {
-    res.send('PUT /places/:id stub')
+    res.send('PUT /places/:id')
 })
 
 router.delete('/:id', (req, res) => {
-    res.send('DELETE /places/:id stub')
+    res.send('DELETE /places/:id')
 })
 
 router.get('/:id/edit', (req, res) => {
-    res.send('GET /places/:id/edit stub')
+    res.send('GET /places/:id/edit')
 })
 
 router.post('/:id/rant', (req, res) => {
-    res.send('GET /places/:id/rant stub')
+    res.send('GET /places/:id/rant')
 })
 
 router.delete('/:id/rant/:rantId', (req, res) => {
-    res.send('GET /places/:id/rant/:rantId stub')
+    res.send('GET /places/:id/rant/:rantId')
 })
 
 
