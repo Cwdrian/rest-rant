6d38b1716b0506b057bd3b3d86f8ea8eae064aab
Cwdrian <xcharlesdrian@gmail.com>
2023-11-13 21:28:50 -0500

    updatet

--- models/index.js
+++ models/index.js
@@ -1,11 +1,15 @@
-require('dotenv').config()
-const mongoose = require('mongoose')
+// models/index.js
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true, 
-  useUnifiedTopology: true
-})
+require("dotenv").config();
+const mongoose = require("mongoose");
+const MONGO_URI = process.env.MONGO_URI;
 
-module.exports.Place = require('./places')
-module.exports.Comment = require('./comment')
+const connect = async () => {
+  await mongoose.connect(MONGO_URI);
+  console.log("connected to Mongo");
+};
+
+module.exports = connect;
+module.exports.Place = require('./places');
+module.exports.Comment = require('./comment');
 
